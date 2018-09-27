f009857ae2db4c618e7cd7d3ec4f91c8febbc375
Lupino <lmjubuntu@gmail.com>
2018-09-27 11:52:26 +0800

    Add C FFI

---
+++ src/Effect/Ref.h
@@ -0,0 +1,11 @@
+#ifndef Effect_Ref_H
+#define Effect_Ref_H
+
+#include <purescript.h>
+
+PURS_FFI_EXPORT(Effect_Ref_new);
+PURS_FFI_EXPORT(Effect_Ref_read);
+PURS_FFI_EXPORT(Effect_Ref_modify$);
+PURS_FFI_EXPORT(Effect_Ref_write);
+
+#endif // Effect_Ref_H
